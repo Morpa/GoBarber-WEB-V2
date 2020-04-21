9d9f954855ca75bf370b8404e33621de1d986608
Andre Morpa <andremorpa@gmail.com>
2020-04-21 19:10:32 +0100

    feat(signup): add signup and styles

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="Logo GoBarber" />
+
+        <form>
+          <h1>Faça o seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+
+          <Input name="email" icon={FiMail} placeholder="Email" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
