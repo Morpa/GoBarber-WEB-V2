4e8af3ddecad0e69046365f986298d683bb2c4a5
Andre Morpa <andremorpa@gmail.com>
2020-04-22 20:00:02 +0100

    feat(app): add and style toast

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn';
 /* import SignUp from './pages/SignUp';
  */
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
@@ -12,6 +13,7 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
