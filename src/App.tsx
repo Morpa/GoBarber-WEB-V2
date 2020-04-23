7f618ae182016ded1e706e9bd1edffcd432aeed1
Andre Morpa <andremorpa@gmail.com>
2020-04-23 09:16:44 +0100

    refactor(app): add custom toasts

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,14 @@ import SignIn from './pages/SignIn';
 /* import SignUp from './pages/SignUp';
  */
 
-import ToastContainer from './components/ToastContainer';
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <SignIn />
-    </AuthProvider>
-    <ToastContainer />
+    </AppProvider>
+
     <GlobalStyle />
   </>
 );
